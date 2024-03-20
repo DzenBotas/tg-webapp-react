0c902538e2ab235b448a53d7b4fefbee922c268a
Dzenbotas <dzenbotas@gmail.com>
2024-03-20 14:22:16 +0200

    fix Form dep

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -15,20 +15,20 @@ const Form = () => {
             subject
         }
         tg.sendData(JSON.stringify(data));
-    }, [country, street, subject])
+    }, [country, street, subject, tg])
 
     useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData)
         return () => {
             tg.offEvent('mainButtonClicked', onSendData)
         }
-    }, [onSendData])
+    }, [onSendData, tg])
 
     useEffect(() => {
         tg.MainButton.setParams({
             text: 'Send'
         })
-    }, [])
+    }, [tg.MainButton])
 
     useEffect(() => {
         if(!street || !country) {
