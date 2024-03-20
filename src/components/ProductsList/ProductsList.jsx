1a50ed74e9f6c4b8282c91b8730c7fdf7eec575b
Dzenbotas <dzenbotas@gmail.com>
2024-03-20 14:12:29 +0200

    add FOrm

---
+++ src/components/ProductsList/ProductsList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ProductsList.css';
+const ProductList = () => {
+    return (
+        <div>
+            ProductList
+        </div>
+    );
+};
+export default ProductList;
