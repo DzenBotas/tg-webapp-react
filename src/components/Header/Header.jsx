9ebdc76f27ab28254be464e7ea3e7ee7e086c81e
Dzenbotas <dzenbotas@gmail.com>
2024-03-20 13:22:36 +0200

    fix Header export

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 // import Button from "../button/button";
 import './Header.css';
 import { useTelegram } from '../../hooks/useTelegram';
-export default Header;
 
 const Header = () => {
-
+    
     const { onClose, user } = useTelegram();
-
+    
     return (
         <div className={'header'}>
             <button onClick={onClose}>Close</button>
@@ -15,3 +14,5 @@ const Header = () => {
         </div>
     );
 };
+
+export default Header;
