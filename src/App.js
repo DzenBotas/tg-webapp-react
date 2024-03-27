a1e45a0b0016146c98610dd294c39f865054f7c4
Dzenbotas <dzenbotas@gmail.com>
2024-03-27 21:31:36 +0200

    Disable Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './hooks/useTelegram';
-import Header from "./components/Header/Header";
+// import Header from "./components/Header/Header";
 import { Route, Routes } from 'react-router-dom';
 import ProductList from './components/ProductsList/ProductsList';
 import Devices from './components/Devices/Devices'; // adjust the path as needed
@@ -18,7 +18,7 @@ function App() {
 
     return (
       <div className="App">
-        <Header />
+        {/* <Header /> */}
         <Routes>
           <Route index element={< ProductList />} />
           <Route path="/devices" element={<Devices />} />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-// import BackButton from "../Button/BackButton";
 import './Header.css';
 
 const Header = () => {
    
     return (
         <div className={'header'}>
-            {/* <BackButton>Back</BackButton> */}
-            {/* <Button onClick={onExpand}>Expand</Button> */}
             {/* <span className={'username'}>{user?.username}</span> */}
             <h3>Travel Bundles</h3>
         </div>
