0f1bfca8a6d6f5196be99770c6a61b29c2e4d6ca
Dzenbotas <dzenbotas@gmail.com>
2024-03-27 21:04:02 +0200

    move Back button to devices from header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import BackButton from "../Button/BackButton";
+// import BackButton from "../Button/BackButton";
 import './Header.css';
 
 const Header = () => {
    
     return (
         <div className={'header'}>
-            <BackButton>Back</BackButton>
+            {/* <BackButton>Back</BackButton> */}
             {/* <Button onClick={onExpand}>Expand</Button> */}
             {/* <span className={'username'}>{user?.username}</span> */}
             <h3>Travel Bundles</h3>
