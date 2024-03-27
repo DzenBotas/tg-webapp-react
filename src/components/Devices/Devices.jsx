191b3406db9f2efee88f372a5997424dca5d6379
Dzenbotas <dzenbotas@gmail.com>
2024-03-27 13:15:11 +0200

    fix tab list err

--- src/components/Devices/Devices.jsx
+++ src/components/Devices/Devices.jsx
@@ -17,25 +17,43 @@ function Devices() {
                 <li className={activeTab === 'tablets-laptops' ? 'active' : ''} onClick={() => setActiveTab('tablets-laptops')}>Tablets & Laptops</li>
             </ul>
 
-            {activeTab === 'smartphones' && <div>{/* Display smartphones data here */}</div>}
-            {activeTab === 'tablets-laptops' && <div>{/* Display tablets & laptops data here */}</div>}
-            
-            {data.map((deviceType, index) => (
-                <div key={index}>
-                    <h2 className={'type'}>{deviceType.type}</h2>
-                    {deviceType.brands.map((brand, brandIndex) => (
-                        <div key={brandIndex}>
-                            <h3 className={'brand-title'}>{brand.title}</h3>
-                            <ul className={'models'}>
-                                {brand.models.map((model, modelIndex) => (
-                                    <li key={modelIndex}>{model}</li>
-                                ))}
-                            </ul>
-                            <div className={'exceptions'}><span>Exceptions:</span> {brand.exceptions}</div>
-                        </div>
-                    ))}
-                </div>
-            ))}
+            {activeTab === 'smartphones' && 
+                data.filter(deviceType => deviceType.type === 'smartphones').map((deviceType, index) => (
+                    <div key={index}>
+                        <h2 className={'type'}>{deviceType.type}</h2>
+                        {deviceType.brands.map((brand, brandIndex) => (
+                            <div key={brandIndex}>
+                                <h3 className={'brand-title'}>{brand.title}</h3>
+                                <ul className={'models'}>
+                                    {brand.models.map((model, modelIndex) => (
+                                        <li key={modelIndex}>{model}</li>
+                                    ))}
+                                </ul>
+                                <div className={'exceptions'}><span>Exceptions:</span> {brand.exceptions}</div>
+                            </div>
+                        ))}
+                    </div>
+                ))
+            }
+
+            {activeTab === 'tablets-laptops' && 
+                data.filter(deviceType => deviceType.type === 'tablets-laptops').map((deviceType, index) => (
+                    <div key={index}>
+                        <h2 className={'type'}>{deviceType.type}</h2>
+                        {deviceType.brands.map((brand, brandIndex) => (
+                            <div key={brandIndex}>
+                                <h3 className={'brand-title'}>{brand.title}</h3>
+                                <ul className={'models'}>
+                                    {brand.models.map((model, modelIndex) => (
+                                        <li key={modelIndex}>{model}</li>
+                                    ))}
+                                </ul>
+                                <div className={'exceptions'}><span>Exceptions:</span> {brand.exceptions}</div>
+                            </div>
+                        ))}
+                    </div>
+                ))
+            }
         </div>
     );
 }
