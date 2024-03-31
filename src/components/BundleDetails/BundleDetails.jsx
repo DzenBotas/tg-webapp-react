a0c6279312e63387b6c5fe945bb7eb2bb3782c70
Dzenbotas <dzenbotas@gmail.com>
2024-03-31 12:07:17 +0300

    Disable pass title func. Structure html

--- src/components/BundleDetails/BundleDetails.jsx
+++ src/components/BundleDetails/BundleDetails.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+// import { useLocation } from 'react-router-dom';
 import "./BundleDetails.css"
 
 const BundleDetails = () => {
     const [data, setData] = useState(null);
-    const location = useLocation();
-    const title = location.state ? location.state.title : 'Default Title';
-// TODO: title check to pass data to fetch data: ['bundle.{title}']. Use switch?
+    // const location = useLocation();
+    // const title = location.state ? location.state.title : 'Default Title';
+    // TODO: title check to pass data to fetch data: ['bundle.{title}']. Use switch?
     useEffect(() => {
         const fetchData = async () => {
             const response = await fetch('https://myaccount.getesim.io/api/v2/search_bundles', {
@@ -31,41 +31,48 @@ const BundleDetails = () => {
         fetchData();
     }, []);
 
-        return (
-            <div>
-                {data && data.bundles.map((bundle, index) => {
-                    // console.log(bundle.networks); // Log bundle.networks for each bundle
+    return (
+        <div>
+            {data && data.bundles.map((bundle, index) => {
+                // console.log(bundle.networks); // Log bundle.networks for each bundle
 
-                    return (
-                        <React.Fragment key={index}>
-                            <h1>{title}</h1>
-                            <div>{bundle.title}</div>
-                            <div><img src={bundle.img} alt={bundle.title} /></div>
-                            <div>{bundle.description}</div>
-                            {/* <div>{bundle.coverage}</div> */}
-                            <div>{bundle.ip_location}</div>
-                            {/* <div>
-                                {bundle.networks && bundle.networks.map((network, index) => (
-                                    <div key={index}>{network.title}</div>
-                                ))}
-                            </div>                             */}
-                            {bundle.refills && Object.values(bundle.refills).map((refill, index) => (
-                                <div className="gb-price">
-                                    <React.Fragment key={index}>
-                                        <div>{refill.title}</div>
-                                        <div>{refill.price_eur}</div>
-                                        {/* <div>{refill.amount_mb}</div>
-                                        <div>{refill.days}</div>
-                                        <div>{refill.price_usd}</div> */}
-                                    </React.Fragment>
+                return (
+                    <React.Fragment key={index}>
+                        <div className="bundle">
+                            {/* <h1>{title}</h1> */}
+                            <div className="bundle-header">
+                                <div className='bundle-title'>{bundle.title}</div>
+                                <div className='bundle-image'><img src={bundle.img} alt={bundle.title} /></div>
                             </div>
-                            ))}
-                        </React.Fragment>
-                    );
-                })}
-            </div>
-        );
-    };
+                            <div className='bundle-description'>{bundle.description}</div>
+                            {/* <div>{bundle.coverage}</div> */}
+                            <div>Private IP: {bundle.ip_location}</div>
+                            {/* <div>
+                                    {bundle.networks && bundle.networks.map((network, index) => (
+                                        <div key={index}>{network.title}</div>
+                                    ))}
+                                </div>                             */}
+                            <ul className='bundle-plans'>
+                                {bundle.refills && Object.values(bundle.refills).map((refill, index) => (
+                                    <React.Fragment key={index}>
+                                        <li className="bundle-price">
+                                            <div>{refill.title}</div>
+                                            <div>{refill.price_eur} EUR</div>
+                                        </li>
+                                        {/* <div>{refill.amount_mb}</div>
+                                            <div>{refill.days}</div>
+                                            <div>{refill.price_usd}</div> */}
+                                    </React.Fragment>
+                                ))
+                                }
+                            </ul>
+                        </div>
+                    </React.Fragment>
+                );
+            })}
+        </div>
+    );
+};
 
 export default BundleDetails;
 
