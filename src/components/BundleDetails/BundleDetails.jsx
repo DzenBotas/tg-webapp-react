29760f7dec7256e3f8a238c53f381468b308ae6d
Dzenbotas <dzenbotas@gmail.com>
2024-04-01 15:23:03 +0300

    disable button

--- src/components/BundleDetails/BundleDetails.jsx
+++ src/components/BundleDetails/BundleDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 // import { useLocation } from 'react-router-dom';
 import "./BundleDetails.css"
-import Button from "../Button/Button";
+// import Button from "../Button/Button";
 
 const BundleDetails = () => {
     const [data, setData] = useState(null);
