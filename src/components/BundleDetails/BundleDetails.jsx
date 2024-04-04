12d9f624e51e8c7967cef485bfb945c5473b5779
Dzenbotas <dzenbotas@gmail.com>
2024-04-04 11:50:01 +0300

    bg test

--- src/components/BundleDetails/BundleDetails.jsx
+++ src/components/BundleDetails/BundleDetails.jsx
@@ -36,7 +36,7 @@ const BundleDetails = ({ bundle }) => {
 
     return (
         // <Container style={{ backgroundColor: 'var(--tg-theme-secondary-bg-color)' }}>
-        <Container >
+        <div >
             {data && data.bundles.map((bundle, index) => {
                 const networks = bundle.networks.map((network, index) => (
                     <div key={index}>
@@ -87,7 +87,7 @@ const BundleDetails = ({ bundle }) => {
                     </React.Fragment>
                 );
             })}
-        </Container>
+        </div>
     );
 };
 
