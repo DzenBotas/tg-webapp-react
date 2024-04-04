eccb0924faa028e0fef45307cb5c572f8ee89a05
Dzenbotas <dzenbotas@gmail.com>
2024-04-04 12:30:34 +0300

    add module classes

--- src/components/BundleDetails/BundleDetails.jsx
+++ src/components/BundleDetails/BundleDetails.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import "./BundleDetails.css"
+import classes from "./BundleDetails.module.css"
 // import Button from "../Button/Button";
 import { Button, Table, Accordion, Space } from '@mantine/core';
 
-const BundleDetails = ({ bundle }) => {
+const BundleDetails = () => {
     const [data, setData] = useState(null);
     const location = useLocation();
     const title = location.state ? location.state.title : 'Default Title';
@@ -48,7 +48,7 @@ const BundleDetails = ({ bundle }) => {
                     <React.Fragment key={index}>
                         {/* <div className="bundle" style={{ backgroundColor: 'var(--tg-theme-bg-color)' }}> */}
                         <div className="bundle" >
-                            <div className="bundle-header">
+                            <div className="classes:bundle-header">
                                 {/* <div className='bundle-title'>{bundle.title}</div> */}
                                 <div className='bundle-title'>{title}</div>
                                 <div className='bundle-image'><img src={bundle.img} alt={bundle.title} /></div>
@@ -57,17 +57,17 @@ const BundleDetails = ({ bundle }) => {
                             {/* <div>{bundle.coverage}</div> */}
                             <Space h="lg" />
                             <div className='bundle-ip'>Private IP: {bundle.ip_location}</div>
-                                <Accordion variant="filled" radius="md" className='bundle-coverage'>
+                                <Accordion variant="filled" radius="md" className='classes:bundle-coverage'>
                                     <Accordion.Item value="Coverage" >
                                         {/* <Accordion.Control style={{ color: 'var(--tg-theme-text-color)'}}>Coverage</Accordion.Control> */}
                                         <Accordion.Control >Coverage</Accordion.Control>
                                         <Accordion.Panel>{networks}</Accordion.Panel>
                                     </Accordion.Item>
                                 </Accordion>
-                            <Table className='bundle-plans' horizontalSpacing="xl">
+                            <Table className='classes:bundle-plans' horizontalSpacing="xl">
                                 {bundle.refills && Object.values(bundle.refills).map((refill, index) => (
                                     <React.Fragment key={index}>
-                                        <Table.Tr className="bundle-price">
+                                        <Table.Tr className="classes:bundle-price">
                                             <Table.Td>{refill.title}</Table.Td>
                                             <Table.Td>{refill.price_eur} EUR</Table.Td>
                                         </Table.Tr>
