e77b7d17c21d04d88b5037d809a2d0cfd482ce5d
Dzenbotas <dzenbotas@gmail.com>
2024-04-05 16:28:45 +0300

    body bg-color set

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { createTheme, MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const theme = createTheme({
