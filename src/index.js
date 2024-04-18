536519514e5d211afebc8e8b307331807d2b3c6b
Dzenbotas <dzenbotas@gmail.com>
2024-04-18 22:22:33 +0300

    set colors to App.css

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const theme = createTheme({
     /** Put your mantine theme override here */
-    backgroundColor: 'var(--tg-theme-secondary-bg-color)',
-    color: 'var(--tg-theme-text-color)',
+    // backgroundColor: 'var(--tg-theme-secondary-bg-color)',
+    // color: 'var(--tg-theme-text-color)',
     fontFamily: "'Inter', -apple-system, 'BlinkMacSystemFont', system-ui, 'Roboto', 'Segoe UI', 'Helvetica Neue', sans-serif"
   });
 
