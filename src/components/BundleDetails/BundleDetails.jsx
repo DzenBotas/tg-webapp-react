2b3ca8f639b420a9b4207abcfa59caa60d416a5b
Dzenbotas <dzenbotas@gmail.com>
2024-04-18 23:14:43 +0300

    rm bg from root div

--- src/components/BundleDetails/BundleDetails.jsx
+++ src/components/BundleDetails/BundleDetails.jsx
@@ -36,7 +36,7 @@ const BundleDetails = () => {
 
     return (
         // <Container style={{ backgroundColor: 'var(--tg-theme-secondary-bg-color)' }}>
-        <div style={{ backgroundColor: 'var(--tg-theme-secondary-bg-color)' }}>
+        <div >
             {data && data.bundles.map((bundle, index) => {
                 const networks = bundle.networks.map((network, index) => (
                     <div key={index} className={classes.country}>
