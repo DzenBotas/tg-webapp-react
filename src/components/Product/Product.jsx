a42f10e86fcddaebb38aea5e825c24f6565e2b8b
Dzenbotas <dzenbotas@gmail.com>
2024-07-08 18:31:09 +0300

    Product- disable some elements

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+// import { Link, useNavigate } from 'react-router-dom';
 import './Product.css';
 
 const titleToFilename = (title) => {
@@ -10,24 +10,24 @@ const Product = ({product, className }) => {
 
     const imageUrl = `https://vps-ed04f1ea.vps.ovh.net/images/${titleToFilename(product.title)}`;
 
-const navigate = useNavigate();
-const handleOnClick = (event) => {
-    event.preventDefault();
-    navigate("/bundle_details", { state: { title: product.title }});
-    }
+// const navigate = useNavigate();
+// const handleOnClick = (event) => {
+//     event.preventDefault();
+//     navigate("/bundle_details", { state: { title: product.title }});
+//     }
 
     return (
         <div className={'product ' + className}>
             <img className={'img'} src={imageUrl} alt={product.title}/>
             <div className={'title'}><strong>{product.title}</strong></div>
-            <div className={'description'}>{product.description}</div>
+            {/* <div className={'description'}>{product.description}</div> */}
             <div className={'price'}>
                 <span><b>{product.price}</b></span>
             </div>
-            <div className="info-links">
+            {/* <div className="info-links">
                 <Link to="/devices">Supported devices</Link>
                 <a href="/bundle_details" onClick={handleOnClick}>See details</a>
-            </div>
+            </div> */}
         </div>
     );
 };
